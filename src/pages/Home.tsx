2c455db491534b233f422d832076a0baa33ec345
Arthur Almeida <arthurdosantosalmeida@gmail.com>
2021-06-18 21:30:50 -0300

    feat: cria as funções responsáveis pelo gerenciamento das tarefas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,18 +11,38 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task if it's not empty
+    if (!newTaskTitle) {
+      return;
+    }
+
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    };
+
+    setTasks((previousState) => [...previousState, newTask]);
   }
 
   function handleMarkTaskAsDone(id: number) {
-    //TODO - mark task as done if exists
+    const newTasks = tasks.map((task) => (
+      task.id === id
+        ? {
+          ...task,
+          done: !task.done,
+        } : task
+    ));
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const filteredTasks = tasks.filter((task) => task.id !== id);
+
+    setTasks(filteredTasks);
   }
 
   return (
@@ -32,9 +52,9 @@ export function Home() {
       <TodoInput addTask={handleAddTask} />
 
       <MyTasksList 
-        tasks={tasks} 
-        onPress={handleMarkTaskAsDone} 
-        onLongPress={handleRemoveTask} 
+        tasks={tasks}
+        onPress={handleMarkTaskAsDone}
+        onLongPress={handleRemoveTask}
       />
     </>
   )
